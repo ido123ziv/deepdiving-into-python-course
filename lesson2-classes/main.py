11f595d50eb23711e7dada9b1bab0e47aaf6196e
ido.ziv <ido.ziv@kaltura.com>
2022-12-20 13:59:03 +0200

    improving main

--- lesson2-classes/main.py
+++ lesson2-classes/main.py
@@ -6,12 +6,8 @@ Sch = School()
 for j in range(1, 5):
     Sch.add_student(Student(str(j), j, [x+j for x in range(1, 5)]))
     print(Sch.get_student_list()[j-1])
-    # for i in range(10):
-    #     Sch.get_student_list()[j-1].add_grade(j+i)
-    #     # s.add_grade(i**j)
 Sch.get_students()
-"""
+
 print("sorting !")
 Sch.sort_by_avg(True)
 Sch.get_students()
-"""
